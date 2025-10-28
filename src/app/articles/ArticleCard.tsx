e2407fd3826ddad958ee9164046addad2854ed95
Moses Martins <mmoses061@gmail.com>
2025-10-28 11:26:53 +0100

    Add a new article and add links to redirect to details of the article

--- src/app/articles/ArticleCard.tsx
+++ src/app/articles/ArticleCard.tsx
@@ -1,12 +1,13 @@
-export default function ArticleCard({title, imagesrc, subtitle, date, readTime}: {title: string, imagesrc:string, subtitle: string, date: string, readTime: string}) {
+export default function ArticleCard({ title, imagesrc, subtitle, date, readTime, articleLink }: { title: string, imagesrc: string, subtitle: string, date: string, readTime: string, articleLink: string }) {
     return (
-            <div className="w-[80%] max-w-300 max-sm:w-[90%] mx-auto mb-[40px] p-6 bg-gradient-to-b from-[#8A436B] to-[#633B66] text-white rounded-xl shadow-lg hover:shadow-xl transition-shadow duration-300">
+        <a href={articleLink} className="block hover:scale-102 w-[80%] max-w-300 max-sm:w-[90%] mx-auto mb-[40px] p-6 bg-gradient-to-b from-[#8A436B] to-[#633B66] text-white rounded-xl shadow-lg hover:shadow-xl transition-shadow duration-300">
+            
                 <div className="flex flex-col md:flex-row items-start md:items-center md:space-x-8 space-y-6 md:space-y-0">
 
                     {/* Text section */}
                     <div className="flex-grow">
                         <h1 className="text-3xl font-bold mb-3 leading-tight">
-                           {title}
+                            {title}
                         </h1>
 
                         <p className="text-gray-200 mb-5 leading-relaxed">
@@ -24,14 +25,15 @@ export default function ArticleCard({title, imagesrc, subtitle, date, readTime}:
                     <div className="flex-shrink-0 relative max-md:w-[100%] max-sm:w-[100%]">
                         {/* eslint-disable-next-line @next/next/no-img-element */}
                         <img
-                            className="block max-md:w-full max-sm:w-full w-48 h-40 object-fill rounded-lg shadow-md hover:scale-105 transition-transform duration-300"
+                            className="block max-md:w-full max-sm:w-full w-48 h-40 object-fill rounded-lg shadow-md transition-transform duration-300"
                             src={imagesrc}
                             alt="Preview image"
                         />
                     </div>
 
                 </div>
-            </div>
+            
+        </a>
 
     )
 
--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -10,8 +10,9 @@ export default function Articles() {
             <Title name="Articles" />
             <main className="flex-grow">
                 <section className="mt-7.5 mb-27">
-                    <ArticleCard title="Understanding the bind method in JavaScript and why it matters." imagesrc="/BindLogo.webp" subtitle="A Deep Dive into Function Context and Permanent Binding." date="Sep 23, 2025" readTime="3 min" />
-                    <ArticleCard title="Building a Neural Network Framework for Solving PDE" imagesrc="/PDELogo.gif" subtitle="Practical Implementation Guide in TensorFlow" date="Aug 28, 2024" readTime="5 min" />
+                    <ArticleCard title="Understanding Div Soup in HTML and How to Avoid It" imagesrc="/divsoup.webp" subtitle="The Semantic Fix for Code Clutter" date="Oct 28, 2025" readTime="4 min" articleLink="https://medium.com/@mosesmartins/understanding-div-soup-in-html-and-how-to-avoid-it-a2fee375f9e2" />
+                    <ArticleCard title="Understanding the bind method in JavaScript and why it matters." imagesrc="/BindLogo.webp" subtitle="A Deep Dive into Function Context and Permanent Binding." date="Sep 23, 2025" readTime="3 min" articleLink="https://medium.com/@mosesmartins/understanding-the-bind-method-in-javascript-and-why-it-matters-189691b1e289" />
+                    <ArticleCard title="Building a Neural Network Framework for Solving PDE" imagesrc="/PDELogo.gif" subtitle="Practical Implementation Guide in TensorFlow" date="Aug 28, 2024" readTime="5 min" articleLink="https://medium.com/@mosesmartins/building-a-neural-network-framework-for-solving-partial-differential-equations-a0415b216052" />
                 </section>
             </main>
             <Footer />
