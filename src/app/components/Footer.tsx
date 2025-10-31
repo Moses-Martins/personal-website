a397cc6b2816ffa1c8a9f2eaf991183c053a2fc2
Moses Martins <mmoses061@gmail.com>
2025-10-31 23:41:40 +0100

    Add links to Socials

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -12,16 +12,16 @@ export default function Footer() {
           <div>
             <p className="text-lg font-bold max-sm:text-sm">Full-Stack Developer</p>
               
-            <div><span className="font-bold text-base max-sm:text-sm">Email:</span><span className="font-light ml-2 max-sm:text-sm">mmoses061@gmail.com</span></div>
+            <div><span className="font-bold text-base max-sm:text-sm">Email:</span><a href="mailto:mmoses061@gmail.com" className="font-light ml-2 max-sm:text-sm">mmoses061@gmail.com</a></div>
               
           </div>
-          <div className="flex flex-col font-bold justify-between">
+          <div className="flex flex-col font-bold items-center justify-between">
             <span className="max-sm:text-sm">Socials</span>
             <div className="flex items-start justify-evenly">
-              <i className="bi bi-github block m-[10%] max-sm:m-[3%]"></i>
-              <i className="bi bi-linkedin block m-[10%] max-sm:m-[3%]"></i>
-              <i className="bi bi-medium block m-[10%] max-sm:m-[3%]"></i>
-            </div>
+              <a href="https://github.com/Moses-Martins" className=" block mx-3 mt-0.5 max-sm:mx-2"><i className="bi bi-github"></i></a>
+              <a href="https://www.linkedin.com/in/moses-martins/" className=" block mx-3 mt-0.5 max-sm:mx-3"><i className="bi bi-linkedin"></i></a>
+              <a href="https://medium.com/@mosesmartins" className=" block mx-3 mt-0.5 max-sm:mx-2"><i className="bi bi-medium"></i></a>
+            </div> 
           </div>
         </div>
 
