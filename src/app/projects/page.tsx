15a6d521ade9613ee3343c1a79e48873ea4b7a27
Moses Martins <mmoses061@gmail.com>
2025-11-01 13:46:06 +0100

    Change the value of the max width

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -9,7 +9,7 @@ export default function Projects() {
             <Header />
             <Title name="Projects" />
             <main className="flex-grow">
-                <section className="w-[90%] max-w-500 mx-auto mt-7.5 mb-27.5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 max-sm:gap-10 max-sm:mb-20">
+                <section className="w-[90%] max-w-[1214px] mx-auto mt-7.5 mb-27.5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 max-sm:gap-10 max-sm:mb-20">
                     <ProjectCard imageSrc="/personal-website-img.png" tools="Next.Js TypeScript TailwindCSS" name="mosesmartins.com" details="My personal website" live="https://www.mosesmartins.com/" github="https://github.com/Moses-Martins/personal-website" />
                     <ProjectCard imageSrc="/BrowserExtensionLogo.jpg" tools="TypeScript Vite TailwindCSS" name="Browser Extensions" details="Frontend for browser add-ons" live="https://browser-extension-project.netlify.app/" github="https://github.com/Moses-Martins/Browser-extension-manager-UI" />
                     <ProjectCard imageSrc="/Hireloop.png" tools="Golang GorillaMux Postgres S3Bucket" name="HireLoop" details="REST API for a modern Job platform" live="https://hireloop.onrender.com/swagger/index.html" github="https://github.com/Moses-Martins/HireLoop" />
